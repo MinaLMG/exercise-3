65eab0c2b7fed7354b7c03e3a83f980ee2ec0d42
MinaLMG <beandro6@gmail.com>
2022-08-14 05:09:28 +0200

    restructured my files so that each file contains its requests and handled sending objects (categories, ingredients and instructions) between recipe pages

--- exercise-3 frontend/exercise-3 frontend/Pages/CategoryModel.cs
+++ exercise-3 frontend/exercise-3 frontend/Pages/CategoryModel.cs
@@ -15,9 +15,11 @@ namespace exercise_3_frontend.Pages
         [BindProperty(SupportsGet = true)]
         public string Msg { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string ID { get; set; }
-        [BindProperty (SupportsGet =true)]
+        public Guid ID { get; set; }
+        [BindProperty(SupportsGet = true)]
         public string Name { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Open { get; set; } 
         private readonly ILogger<IndexModel> _logger;
         private readonly IConfiguration Configuration;
         public CategoryModel(ILogger<IndexModel> logger, IConfiguration configuration)
@@ -41,15 +43,59 @@ namespace exercise_3_frontend.Pages
             List<Category> categories = JsonSerializer.Deserialize<List<Category>>(inBetween, serializeOptions);
             this.Categories = categories;
         }
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPostAddCategory()
         {
             Category toAdd = new Category(Name);
             var temp = JsonSerializer.Serialize(toAdd);
             var res = await HttpClient.PostAsync(Configuration["BaseUrl"] + "categories", new StringContent(temp, Encoding.UTF8, "application/json"));
             if ((int)res.StatusCode == 200)
+            {
+                //ReqResult = "success";
+                //Msg = "your category has been added successfully";
                 return Redirect("/Categories?ReqResult=success&Msg=your category has been added successfully");
+            }
             else
-                return Redirect("/Categories?ReqResult=failure&Msg=something went wrong with your request .. check your data and try again&name="+Name);
+            {
+                //ReqResult = "failure";
+                //Msg = "something went wrong with your request .. check your data and try again";
+                return Redirect("/Categories?ReqResult=failure&Msg=something went wrong with your request .. check your data and try again&name=" + Name);
+            }
+        }
+        public async Task<IActionResult> OnPostUpdateCategory()
+        {
+            Category toEdit = new Category(Name);
+            toEdit.ID = ID;
+            var temp = JsonSerializer.Serialize(toEdit);
+            var res = await HttpClient.PutAsync(Configuration["BaseUrl"] + "categories/" + ID, new StringContent(temp, Encoding.UTF8, "application/json"));
+            if ((int)res.StatusCode == 200)
+            {
+                //ReqResult = "success";
+                //Msg = "the category has been updated successfully";
+                return Redirect("/Categories?ReqResult=success&Msg=the category has been updated successfully");
+            }
+            else
+            {
+                //ReqResult = "failure";
+                //Msg = "something went wrong with your request .. review your data and try again";
+                //Open = "edit";
+                return Redirect("/Categories?ReqResult=failure&Msg=something went wrong with your request .. review your data and try again&name=" + Name + "&id=" + ID + "&open=edit");
+            }
+        }
+        public async Task<IActionResult> OnPostDeleteCategory()
+        {
+            var res = await HttpClient.DeleteAsync(Configuration["BaseUrl"] + "categories/" + ID);
+            if ((int)res.StatusCode == 200)
+            {
+                //ReqResult = "success";
+                //Msg = "the category has been deleted successfully";
+                return Redirect("/Categories?ReqResult=success&Msg=the category has been deleted successfully");
+            }
+            else
+            {
+                //ReqResult = "failure";
+                //Msg = "something went wrong with your request .. you can retry after some seconds";
+                return Redirect("/Categories?ReqResult=failure&Msg=something went wrong with your request .. you can retry after some seconds&id=" + ID + "&open=delete");
+            }
         }
     }
 }
